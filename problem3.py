a6cfcc01ef5f3a553525a7bdfc4654bb80b938bc
KennethIs <43756442+KennethIs@users.noreply.github.com>
2018-12-17 12:46:08 -0500

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Alien Greeter Bot: ')
+print()
+planet = input('Where you from fam: ')
+if planet == 'Earth' or  planet == 'earth':
+	print("Oh, well that's awkward you look like a ... Well have a nice day! ")
+
+else:
+	print("Welcome to Earth! Don't know why you picked this planet among the billions of other planets but enjoy your stay if you can!")
+print('-'*60)
